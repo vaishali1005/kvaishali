b734d139fa35c9bc32a641eb8cd08a1dd88734ce
vaishali <vaishali.xyz.com>
2019-03-14 07:22:35 +0000

    mvn file

---
+++ again/vaishali-jar/src/main/java/vaishali/exe/com/App.java
@@ -0,0 +1,13 @@
+package vaishali.exe.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
